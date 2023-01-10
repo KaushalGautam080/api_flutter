237b1bfcb758ac73f66e35fd1660a4c45988ff3d
Kaushal Gautam <kaushalgautam@Kaushals-MacBook-Pro.local>
2023-01-10 11:02:19 +0545

    feat:post list initialized

---
+++ lib/home_screen.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:api_project/Models/PostsModel.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<PostsModel> postList = [];
+
+  Future<List<PostsModel>> getpostApi() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map i in data) {
+        postList.add(PostsModel.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("API"),
+      ),
+      body: Column(children: []),
+    );
+  }
+}
